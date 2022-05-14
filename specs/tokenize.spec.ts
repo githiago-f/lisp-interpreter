c5a0ba84bca4d110f2c42a0aee54b0e0b80da169
githiago-f <tfarias@protonmail.com>
2022-05-14 03:52:36 +0000

    creating code basic verification before tokenization

--- specs/tokenize.spec.ts
+++ specs/tokenize.spec.ts
@@ -1,29 +1,33 @@
 import { tokenize } from '../src/tokenize';
-import { expect } from 'chai';
+import { expect, should } from 'chai';
 import { join } from 'path';
 import { readFileSync } from 'fs';
 
 describe('#tokenize', () => {
   context('Valid input', () => {
-    const filePath = join(__dirname, './inputs/comments.lsp');
-    const comments = readFileSync(filePath).toString();
-
-    context('contains string literal', () => {
-      const tokenized = tokenize(comments);
-      it('Does not break strings', () => {
-        const hasString = tokenized.hasValue("\"Hello, World!\"");
-        expect(tokenized.next).not.be.equal(null);
-        expect(hasString).to.be.true;
+    context('tokenizes the input file', () => {
+      let tokenized: string[];
+      beforeEach(() => {
+        const filePath = join(__dirname, './inputs/comments.lsp');
+        const comments = readFileSync(filePath).toString();
+        tokenized = tokenize(comments) as string[];
       });
-    });
-
-    context('has comments (;;)', () => {
-      const tokenized = tokenize(comments);
+      it('doesn\'t break strings', () => {
+        const literalString = tokenized.includes('"Hello, World!"');
+        expect(literalString).is.true;
+      });
       it('ignores commented lines', () => {
-        const hasCommenttedString = tokenized.hasValue(";;");
-        expect(tokenized.value).to.be.equal('(');
-        expect(hasCommenttedString).to.be.false;
+        const commentedLine = tokenized.includes(
+          ';; ignoring lines is easy'
+        );
+        expect(commentedLine).is.false;
       });
     });
+    context('validate the brackets balance', () => {
+      const invalidInput = '(setq x 3';
+      it('dont tokenize if isnt balanced', () => {
+        expect(tokenize(invalidInput)).to.be.false;
+      });
+    })
   });
 });
--- src/tokenize.ts
+++ src/tokenize.ts
@@ -1,6 +1,28 @@
-import { atom } from "./atom";
+import { last } from 'lodash';
+
+export const isBalanced = (input: string) => {
+  const noComands = input.replace(/[^\(\)\[\]\{\}]/gi, '');
+  const chars = noComands.split('');
+  const q = [];
+  for (const char of chars) {
+    if (q.length < 1) {
+      q.push(char);
+    } else if (
+      (last(q) === '(' && char === ')') ||
+      (last(q) === '{' && char === '}') ||
+      (last(q) === '[' && char === ']')
+    ) {
+      q.pop();
+    } else {
+      q.push(char);
+    }
+  }
+  return q.length === 0;
+}
 
 export const tokenize = (input: string) => {
+  if (!isBalanced(input)) return false;
+
   const items = input
     .replace(/\;\;(.*?)\n|\n\r|$/g, '')
     .replace(/\n/g, '')
@@ -11,8 +33,5 @@ export const tokenize = (input: string) => {
     .split(/\s/)
     .map(i => i.replace(/\$space\$/gi, ' '));
 
-  const head = atom(items.shift());
-  items.forEach(i => head.push(atom(i)));
-
-  return head;
+  return items;
 }
